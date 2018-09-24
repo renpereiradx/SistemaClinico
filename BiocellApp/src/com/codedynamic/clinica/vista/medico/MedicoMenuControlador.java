23fc1db92b2d6bc7574142f7808722b16078311a
clinica-pc <rene_dx92@hotmail.com>
2018-09-24 17:27:04 -0400

    Metodo atender, primeros pasos

--- BiocellApp/src/com/codedynamic/clinica/vista/medico/MedicoMenuControlador.java
+++ BiocellApp/src/com/codedynamic/clinica/vista/medico/MedicoMenuControlador.java
@@ -1,5 +1,28 @@
 package com.codedynamic.clinica.vista.medico;
 
-public class MedicoMenuControlador {
+import com.codedynamic.clinica.MainApp;
 
+import javafx.fxml.FXML;
+
+public class MedicoMenuControlador {
+	private MainApp mainApp;
+	
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp;
+	}
+	
+	@FXML
+	private void initialize() {}
+
+	@FXML
+	private void mostrarMedicoPrincipal() {
+		mainApp.mostrarMedicoPrincipal();
+	}
+	
+	@FXML
+	private void mostrarPaginaPrincipal() {
+		mainApp.getContenedorPrincipal().setLeft(null);
+		mainApp.getContenedorPrincipal().setCenter(null);
+		mainApp.showHome();
+	}
 }
