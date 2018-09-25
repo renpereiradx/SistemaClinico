b488f2eebf78bc0b3f386268a6b9cdd4b8a536cd
clinica-pc <rene_dx92@hotmail.com>
2018-09-25 18:09:32 -0400

    Clase para el modelo

---
+++ BiocellApp/src/com/codedynamic/clinica/modelo/Anamnesis.java
@@ -0,0 +1,42 @@
+package com.codedynamic.clinica.modelo;
+
+public class Anamnesis {
+	private short idAnamnesis;
+	private String antecedenteEnfermedad;
+	private String medicamentoActual;
+		
+	public Anamnesis() {
+		super();
+	}
+
+	public Anamnesis(short idAnamnesis, String antecedenteEnfermedad, String medicamentoActual) {
+		super();
+		this.idAnamnesis = idAnamnesis;
+		this.antecedenteEnfermedad = antecedenteEnfermedad;
+		this.medicamentoActual = medicamentoActual;
+	}
+
+	public short getIdAnamnesis() {
+		return idAnamnesis;
+	}
+
+	public void setIdAnamnesis(short idAnamnesis) {
+		this.idAnamnesis = idAnamnesis;
+	}
+
+	public String getAntecedenteEnfermedad() {
+		return antecedenteEnfermedad;
+	}
+
+	public void setAntecedenteEnfermedad(String antecedenteEnfermedad) {
+		this.antecedenteEnfermedad = antecedenteEnfermedad;
+	}
+
+	public String getMedicamentoActual() {
+		return medicamentoActual;
+	}
+
+	public void setMedicamentoActual(String medicamentoActual) {
+		this.medicamentoActual = medicamentoActual;
+	}
+}
