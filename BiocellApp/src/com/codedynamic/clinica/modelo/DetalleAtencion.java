0658517b9c5d113b2a47159d62ac6c6bc67f8ba0
clinica-pc <rene_dx92@hotmail.com>
2018-09-28 17:24:51 -0400

    Modelo Incompleto

---
+++ BiocellApp/src/com/codedynamic/clinica/modelo/DetalleAtencion.java
@@ -0,0 +1,12 @@
+package com.codedynamic.clinica.modelo;
+
+public class DetalleAtencion {
+	private short idDA;
+	private Atencion atencion;
+	private Usuario usuario;
+	private Anamnesis anamnesis;
+	private Habito habito;
+	private ExamenFisico examenFisico;
+	private Ippa ippa;
+	
+}
