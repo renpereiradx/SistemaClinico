718c7f9530d344a2a155b722acc20875d4119b1c
clinica-pc <rene_dx92@hotmail.com>
2018-10-24 17:50:35 -0300

    Refactor

--- BiocellApp/src/com/codedynamic/clinica/dao/postgresql/PSQLFactura.java
+++ BiocellApp/src/com/codedynamic/clinica/dao/postgresql/PSQLFactura.java
@@ -9,7 +9,7 @@ public class PSQLFactura implements FacturaDAO {
 	private final String MODIFICAR = "UPDATE facturas SET id_paciente = ?, id_usuario = ?, fecha = ?, hora = ?, total = ?, estado = ? WHERE id_factura = ?";
 	private final String ELIMINAR = "DELETE FROM facturas WHERE id_factura = ?";
 	private final String OBTENERPORIDPACIENTE = "SELECT id_factura, id_paciente, id_usuario, fecha, hora, total, estado FROM facturas WHERE id_paciente = ?";
-
+	
 	@Override
 	public void insertar(Factura o) {
 		// TODO Auto-generated method stub
